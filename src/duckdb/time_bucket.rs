8d0b7d5f32b20528aaf6230197884993e2f26b32
Devan <devandbenz@gmail.com>
2024-08-03 15:41:17 -0500

    fmt
    Signed-off-by: Devan <devandbenz@gmail.com>

--- src/duckdb/time_bucket.rs
+++ src/duckdb/time_bucket.rs
@@ -15,8 +15,8 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-use pgrx::*;
 use pgrx::iter::TableIterator;
+use pgrx::*;
 
 #[pg_extern(name = "time_bucket")]
 pub fn time_bucket_date_no_offset(
