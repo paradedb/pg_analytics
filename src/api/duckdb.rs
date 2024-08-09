afcd8732ba5ba136f75d462c6cf43b36b3facb12
Vipul Vaibhaw <vaibhaw.vipul@gmail.com>
2024-08-09 10:45:44 +0530

    remove unused imports

--- src/api/duckdb.rs
+++ src/api/duckdb.rs
@@ -1,4 +1,4 @@
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use pgrx::*;
 
 use crate::duckdb::connection;
--- src/api/parquet.rs
+++ src/api/parquet.rs
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use pgrx::*;
 
 use crate::duckdb::utils;
