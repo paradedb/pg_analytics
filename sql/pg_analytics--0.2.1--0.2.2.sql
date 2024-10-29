02e7b4b0af1a55a0155d23fdce0a4796f1701b50
Philippe Noël <philippemnoel@gmail.com>
2024-10-29 11:14:47 +0000

    Upgrade script

--- sql/pg_analytics--0.2.1--0.2.2.sql
+++ sql/pg_analytics--0.2.1--0.2.2.sql
@@ -1,1 +1,8 @@
 \echo Use "ALTER EXTENSION pg_analytics UPDATE TO '0.2.2'" to load this file. \quit
+
+DROP FUNCTION IF EXISTS time_bucket(_bucket_width pg_catalog."interval", _input date);
+DROP FUNCTION IF EXISTS time_bucket(_bucket_width pg_catalog."interval", _input date, _offset pg_catalog."interval");
+DROP FUNCTION IF EXISTS time_bucket(_bucket_width pg_catalog."interval", _input date, _origin date);
+DROP FUNCTION IF EXISTS time_bucket(_bucket_width pg_catalog."interval", _input pg_catalog."timestamp");
+DROP FUNCTION IF EXISTS time_bucket(_bucket_width pg_catalog."interval", _input pg_catalog."timestamp", _origin date);
+DROP FUNCTION IF EXISTS time_bucket(_bucket_width pg_catalog."interval", _input pg_catalog."timestamp", _offset pg_catalog."interval");
